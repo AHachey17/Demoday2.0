59fed4d123809a48119f32efc05722a1579cb71d
AHachey17 <ahachey17@gmail.com>
2023-05-22 14:41:46 -0400

    took out app.listen

--- config/database.js
+++ config/database.js
@@ -1,14 +1,19 @@
 const mongoose = require("mongoose");
+const express = require('express')
 
+const app = express()
+const PORT = process.env.PORT || 5000
 
 const connectDB = async () => {
   try {
+    console.log(process.env.DB_STRING)
     const conn = await mongoose.connect(process.env.DB_STRING, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
       useCreateIndex: true,
     });
+
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
@@ -17,10 +22,6 @@ const connectDB = async () => {
   }
 };
 
-connectDB().then(() => {
-  app.listen(process.env.PORT, () => {
-      console.log("listening for requests");
-  })
-})
+
 
 module.exports = connectDB;
